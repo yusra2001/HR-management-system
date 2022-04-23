83a67248723c7ebb3b24e347e8682ffe787b3b00
yusra2001 <yusrahadidi@gmail.com>
2022-04-23 21:57:15 +0300

    done

---
+++ app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+
+// const employee = {
+
+//    employeeID: '' 
+//     fullName: 'testFirstName',
+//     department: 'testLastName'
+//     level:
+//     imageURL:
+//     salary:
+//   };
+////////////////
+let allEmp = [];
+////// constructor function /////
+function Emp(employeeID,firstName,lastName,department,level,imageURL) {
+    this.employeeID = employeeID;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.department = department;
+    this.level = level;
+    this.imageURL= imageURL;
+     this.salary= this.getsal();
+     this.gettaxx= this.gettax();
+     this.getnetsala= this.getnetsal();
+
+    allEmp.push(this);
+}
+//////////////
+Emp.prototype.gettax=function()
+{
+    var tax 
+    for (var i = 0; i < 7; i++) {
+    tax =this.salary*0.075;
+        }    return tax;
+   }
+// //   //////////
+Emp.prototype.getnetsal=function()
+{    var netsalary 
+   for (var i = 0; i < 7; i++) {
+     netsalary =this.salary-this.gettaxx;
+    }return netsalary;
+
+}
+///////////////////////////////////////
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  Emp.prototype.getsal=function(){
+let rndInt=0;
+  
+  if (this.level=="Senior"){
+  rndInt = randomIntFromInterval(1500, 2000)}
+  else if (this.level=="Mid-Senior"){
+    rndInt = randomIntFromInterval(1000, 1500)
+  }
+  else if (this.level=="Junior"){
+     rndInt = randomIntFromInterval(500, 1000)
+  } console.log(rndInt)
+return rndInt;
+  }
+  //////////////////////
+  let emp1 = new Emp("1000","Ghazi", "Samer","Adminstration", "Senior","/emp1.jpg");
+console.log(emp1);
+////////////
+let emp2 = new Emp("1001","Lana", "Ali","Finance", "Senior","emps/emp2.jpg");
+console.log(emp1);
+let emp3 = new Emp("1002","Tamara", "Ayoub","Marketing", "Senior","emps/emp3.jpg");
+console.log(emp1);
+let emp4 = new Emp("1003","Safi", "Walid","Adminstration", "Mid-Senior","emps/emp4.jpg");
+console.log(emp1);
+let emp5 = new Emp("1004","Omar", "Ziad","Development", "Senior","emps/emp5.jpg");
+console.log(emp1);
+let emp6 = new Emp("1005","Rana", "Saleh","Development", "Junior","emps/emp6.jpg");
+console.log(emp1);
+let emp7 = new Emp("1006","Hadi", "Ahmad","Finance", "Mid-Senior","emps/emp7.jpg");
+console.log(emp1);
+////////////////////////////////////////////////////////////
+// 
+var tag_id = document.getElementById('tagid');
+var newNode = document.createElement('p');
+newNode.appendChild(document.createTextNode('html string'));
+// var tag_id = document.getElementById('tagid');
+// var newNode = document.createElement('p');
+// newNode.appendChild(document.createTextNode('html string'));
+
+  console.log(emp1);
+   console.log(emp2);
+   console.log(emp3);
+   console.log(emp4);
+   console.log(emp5);
+   console.log(emp6);
+
+//   /////////////////
+
+//document.write(emp1);
+///document.write(emp1);
+document.write(this.employeeID);
+document.write(this.firstName);
+document.write(this.salary);
+document.write(allEmp);
+
+
+//ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
